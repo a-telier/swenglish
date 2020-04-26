059af5da9a658485b6ad071ae01851902ce75d66
alex-rabi <Alexandra.rabi@outlook.com>
2020-04-26 20:18:29 +0000

    js not working

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+//Screen 1 - start menu
 $("#button-start").click(function() {
   $("#screen-1").fadeOut('slow');
+});
+
+//Screen 2 - cards game
+$("#button-back").click(function() {
+  $("#screen-1").fadeIn('slow');
+});
+
+$(".cards").mouseenter(function(){
+  $(this).fadeOut();
+});
+
+$(".cards").mouseleave(function(){
+  $(this).fadeIn();
 });
